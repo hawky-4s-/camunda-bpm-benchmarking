bcce1da80b295881b1535c0c1b13a23f3b2d101d
Christian Lipphardt <christian.lipphardt@googlemail.com>
2015-09-03 14:23:41 +0200

    chore(script): improve startup-db script

--- start-db.sh
+++ start-db.sh
@@ -3,10 +3,13 @@ HOST=${1:-ci3.camunda.loc}
 export DOCKER_HOST=tcp://$HOST:2375
 unset DOCKER_TLS_VERIFY
 
+echo "Removing old containers..."
 docker rm -f -v postgresql
 docker rm -f -v cadvisor
 
-sleep 10
+sleep 5
+
+echo "\nStarting postgresql on $HOST:5432"
 
 docker run \
     --cpuset-cpus="0" \
@@ -19,5 +22,15 @@ docker run \
     --name postgresql \
     postgres:9.4.4
 
-docker run -d --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro --publish=8080:8080 --name=cadvisor google/cadvisor:latest -storage_duration=60m0s
+echo "\nStarting cadvisor on http://$HOST:8080"
+
+docker run \
+    -d \
+    --volume=/:/rootfs:ro \
+    --volume=/var/run:/var/run:rw \
+    --volume=/sys:/sys:ro \
+    --volume=/var/lib/docker/:/var/lib/docker:ro \
+    --publish=8080:8080 \
+    --name=cadvisor \
+    google/cadvisor:latest -storage_duration=60m0s
 
