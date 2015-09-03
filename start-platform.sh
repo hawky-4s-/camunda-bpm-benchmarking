fbdf4524d95855d41d70e499da7226622872c9f1
Christian Lipphardt <christian.lipphardt@googlemail.com>
2015-09-03 15:18:17 +0200

    chore(platform): allow dynamic JMX portmapping

--- start-platform.sh
+++ start-platform.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 VERSION=${1:-SNAPSHOT}
-HOST=${2:-ci4.camunda.loc}
-DB_HOST=${3:-ci3.camunda.loc}
+HOST=${2:-192.168.178.107} # ci4.camunda.loc
+JMX_PORT=${3:-9090}
+DB_HOST=${4:-ci3.camunda.loc}
 export DOCKER_HOST=tcp://$HOST:2375
 unset DOCKER_TLS_VERIFY
 
-docker rm -f -v camunda
+docker pull ci1.camunda.loc:5000/camunda/camunda-bpm-platform:tomcat-7.4.0-$VERSION-benchmark
+
+echo "Starting camunda-bpm-platform:"
 
 docker run \
-    -p 8080:8080 \
-    -p 9090:9090 \
+    -p $JMX_PORT:$JMX_PORT \
     -e TZ=Europe/Berlin \
     -e DB_DRIVER=org.postgresql.Driver \
     -e DB_URL=jdbc:postgresql://$DB_HOST:5432/process-engine \
     -e DB_USERNAME=camunda \
     -e DB_PASSWORD=camunda \
+    -e HOST=$HOST \
+    -e JMX_PORT=$JMX_PORT \
     -d \
-    --name camunda \
     ci1.camunda.loc:5000/camunda/camunda-bpm-platform:tomcat-7.4.0-$VERSION-benchmark
+
